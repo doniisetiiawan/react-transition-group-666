0af0dc5237142a466881b8d097d54f4ace6c72b9
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-03 10:22:44 +0700

    Refactor list

--- src/components/Todo/List.jsx
+++ src/components/Todo/List.jsx
@@ -1,18 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
+import './List.css';
 
 const List = props => (
   <ul>
-    {props.items.map((item, key) => (
-      <li key={key}>
-        {item.task}
-      </li>
-    ))}
+    <TransitionGroup>
+      {props.items.map((item, key) => (
+        <CSSTransition
+          classNames="todo"
+          timeout={300}
+        >
+          <li
+            key={key}
+            className={`${item.completed ? 'completed' : 'pending'}`}
+          >
+            {item.task}
+
+            <div className="actions">
+              <span
+                className={item.completed ? 'hide' : 'done'}
+                onClick={() => props.markAsCompleted(item.id)}
+              >
+                <i className="fa fa-check" />
+              </span>
+              <span
+                className="trash"
+                onClick={() => props.removeTask(item.id)}
+              >
+                <i className="fa fa-trash" />
+              </span>
+            </div>
+          </li>
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
   </ul>
 );
 
 List.propTypes = {
   items: PropTypes.array,
+  markAsCompleted: PropTypes.func,
+  removeTask: PropTypes.func,
 };
 
 export default List;
